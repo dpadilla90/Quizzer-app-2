d69c584aad4e403497c45c55014b0d32994f5e9c
Dennis Padilla <d_padilla.y@hotmail.com>
2023-07-05 12:49:24 -0400

    Added questions and navigation between questions

--- app/src/main/java/com/example/quizzer/MainActivity.kt
+++ app/src/main/java/com/example/quizzer/MainActivity.kt
@@ -4,36 +4,59 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.example.quizzer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    //The ActivityMainBinding class is generated from the layout file (activity_main.xml)
+    private lateinit var binding: ActivityMainBinding
+    private var currentQuestionIndex = 0
 
+    private val questions = listOf(
+        R.string.question_france,
+        R.string.question_planet,
+        R.string.question_art
+
+
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        //This makes the views defined in activity_main.xml accessible
+        //for the UI of this activity
+        setContentView(binding.root)
 
-        //'R' is a class in Android that contains the definitions for all the recources
-        //of our application. In this. R.id.ture_button -> a button with the id "true_button"
-        //that is defined in the layout file(specifically activity_main.xml)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT
             ).show()
         }
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
             ).show()
         }
+
+        binding.nextButton.setOnClickListener {
+            currentQuestionIndex = (currentQuestionIndex +1) % questions.size
+            //0 -> 1  % 3 = 1
+            //1 -> 2  % 3 = 2
+            //2 -> 3  % 3 = 0
+            binding.questionTextView.setText(questions[currentQuestionIndex])
+        }
+
+        binding.previousButton.setOnClickListener {
+            currentQuestionIndex = (currentQuestionIndex - 1 + questions.size) % questions.size
+            // 0 -> -1 + 3 = 2 % 3 = 2
+            // 1 ->  0 + 3 = 3 % 3 = 0
+            // 2 ->  1 + 3 = 4 % 3 = 1
+            binding.questionTextView.setText(questions[currentQuestionIndex])
+        }
+
     }
 
 
